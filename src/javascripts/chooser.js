51d14822bf4c9d66cbf77aff1a5bacf9ed82c027
Russell Hickey <rhickeyjnr@gmail.com>
2016-09-08 08:56:26 +0100

    Update to add opacity to Line Chart

--- src/javascripts/chooser.js
+++ src/javascripts/chooser.js
@@ -1,8 +1,18 @@
 $(document).ready(function() {
-    // Chart.defaults.global.defaultFontColor = "#000";
     Chart.defaults.global.defaultFontColor = "#FFF";    
-
-
+    var Options = {
+        line: {
+            hexColor: "#F00",
+            rgbColor: {
+                r: 255,
+                g: 0,
+                b: 0,
+                a: 1
+            },
+            transparency: "0.2"
+        }
+    };
+    
     var ctx = $("#lineChart");
     var bgColor = "rgba(255, 0, 0, 0.2)";
     var borderColor = "rgba(255, 0, 0, 1)";
@@ -25,13 +35,14 @@ $(document).ready(function() {
         data: data
     });
     
-    function updateChartColor(col) {
-        var color = col.toRgb();
+    $("#line-transparency").val(Options.line.transparency);
+    
+    function updateLineChart() {
+        var color = Options.line.rgbColor;
         var c = "rgba("+color.r+","+color.g+","+color.b+",";
-        var fill = c + "0.2)"; // Make it lighter color
+        var fill = c + Options.line.transparency + ")"; // Make it lighter color
         var stroke = c + "1)"; // Make it normal color
-        var hex = col.toHex().toUpperCase();
-        lineColor.innerHTML = '#' + hex;
+        lineColor.innerHTML = Options.line.hexColor;
         lineChart.data.datasets[0].backgroundColor = fill;
         lineChart.data.datasets[0].borderColor = stroke;
         lineChart.update();
@@ -41,7 +52,15 @@ $(document).ready(function() {
         color: '#F00',
         flat: true,
         move: function(color) {
-            updateChartColor(color);
+            Options.line.rgbColor = color.toRgb();
+            Options.line.hexColor = "#" + color.toHex();
+            updateLineChart();
         }
     });
+    
+    $("#line-transparency").bind('keyup mouseup', function () {
+        Options.line.transparency = $(this).val();
+        updateLineChart();
+    });
+
 });
