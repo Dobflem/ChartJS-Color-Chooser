affdb18b92518924e0bd59bc6c7034619bf10db9
Russell Hickey <rhickeyjnr@gmail.com>
2016-09-09 12:04:48 +0100

    Add editable data to Chart

--- src/javascripts/chooser.js
+++ src/javascripts/chooser.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
-    Chart.defaults.global.defaultFontColor = "#FFF";    
+    Chart.defaults.global.defaultFontColor = "#FFF";  
+
+    var ctx = $("#lineChart");
+    var lineColorChooser = document.getElementById("line-color-chooser");
+    var lineColor = document.getElementById("lineColor");
+    
     var Options = {
         currentDataset: 0,
         global: {
@@ -7,64 +12,39 @@ $(document).ready(function() {
         },
         line: [
             {
-                hexColor: "#FF0000",
-                rgbColor: {
-                    r: 255,
-                    g: 0,
-                    b: 0,
-                    a: 1
+                dataset: {
+                    label: "Dataset One",
+                    backgroundColor: "rgba(255, 0, 0, 0.2)",
+                    borderColor: "rgba(255, 0, 0, 1)",
+                    data: [10, 20, 5, 15, 20]
                 },
                 opacity: "0.5",
-                previousRGB: {
-                    r: 255,
-                    g: 0,
-                    b: 0,
-                    a: 1
-                },
+                rgbColor: { r: 255, g: 0, b: 0, a: 1 },
+                previousRGB: { r: 255, g: 0, b: 0, a: 1 },
+                hexColor: "#FF0000",
                 previousHex: "#FF0000"
             },
             {
-                hexColor: "#FF0000",
-                rgbColor: {
-                    r: 255,
-                    g: 0,
-                    b: 0,
-                    a: 1
+                dataset: {
+                    label: "Dataset Two",
+                    backgroundColor: "rgba(255, 0, 0, 0.2)",
+                    borderColor: "rgba(255, 0, 0, 1)",
+                    data: [16, 18, 10, 17, 16]
                 },
                 opacity: "0.5",
-                previousRGB: {
-                    r: 255,
-                    g: 0,
-                    b: 0,
-                    a: 1
-                },
+                rgbColor: { r: 255, g: 0, b: 0, a: 1 },
+                previousRGB: { r: 255, g: 0, b: 0, a: 1 },
+                hexColor: "#FF0000",
                 previousHex: "#FF0000"
             }    
         ]
     };
     
-    var ctx = $("#lineChart");
-    var bgColor = "rgba(255, 0, 0, 0.2)";
-    var borderColor = "rgba(255, 0, 0, 1)";
-    var lineColorChooser = document.getElementById("line-color-chooser");
-    var lineColor = document.getElementById("lineColor");
-    lineColor.style.backgroundColor = Options.line[Options.currentDataset].hexColor;
-    
     var data = {
         labels: ["Demo", "Demo", "Demo", "Demo", "Demo"],
         datasets: [
-            {
-                label: "Dataset One",
-                backgroundColor: bgColor,
-                borderColor: borderColor,
-                data: [10, 20, 5, 15, 20]
-            },
-            {
-                label: "Dataset Two",
-                backgroundColor: bgColor,
-                borderColor: borderColor,
-                data: [16, 18, 10, 17, 16]
-            }
+            Options.line[0].dataset,
+            Options.line[1].dataset
         ]
     };
 
@@ -73,7 +53,30 @@ $(document).ready(function() {
         data: data
     });
     
+    lineColor.style.backgroundColor = Options.line[Options.currentDataset].hexColor;
     $("#line-opacity").val(Options.line[Options.currentDataset].opacity);
+    
+    function recreateLineChart() {
+        var data = {
+                datasets: []
+            },
+            j = Options.line.length,
+            max = 0;
+        
+        for (var i = 0; i < j; i++) {
+            var size = Options.line[i].dataset.data.length;
+            data.datasets[i] = Options.line[i].dataset;
+            max = (max > size) ? max : size; 
+        }
+        
+        data.labels = Array(max).fill("Demo");
+        
+        lineChart.destroy();
+        lineChart = new Chart(ctx, {
+            type: 'line',
+            data: data
+        });
+    }
     
     function getTextColor() {
         var rgb = Options.line[Options.currentDataset].rgbColor;
@@ -95,12 +98,27 @@ $(document).ready(function() {
         }
     }
     
+    function createDataNodes() {
+        var html = "",
+            data = Options.line[Options.currentDataset].dataset.data,
+            len = data.length;
+        
+        for(var i = 0; i < len; i++) {
+            html += '<input class="input line-data" value="'+data[i]+'" type="text">'
+        }
+        
+        html += '<input class="input line-data" value="" type="text">';
+        
+        $("#line-data").html(html);
+    }
+    
     function updatePreferences() {
         var line = Options.line[Options.currentDataset];
         var color = line.hexColor;
         var opacity = line.opacity;
         var points = Options.global.points;
         
+        createDataNodes();
         lineColor.innerHTML = color;
         lineColor.style.backgroundColor = color;
         $("#line-opacity").val(opacity);
@@ -145,6 +163,19 @@ $(document).ready(function() {
         var offsets = $("#lineColor").position();
         lineColorChooser.style.top = (offsets.top - 93) + "px";
         lineColorChooser.style.left = (offsets.left + 193) + "px";
+    }
+    
+    function updateDataset() {
+        var line = Options.line[Options.currentDataset],
+            len = line.dataset.data.length,
+            data = lineChart.data.datasets[Options.currentDataset].data;
+        
+        data.length = len;
+        for(var i = 0; i < len; i++) {
+            data[i] = line.dataset.data[i];
+        }
+        
+        lineChart.update();
     }
     
     $(".spectrum").spectrum({
@@ -196,9 +227,34 @@ $(document).ready(function() {
     
     $("[data-idx]").each(function() {
         $(this).on('click', function() {
+            $("#btn-line-color-save").click();
             Options.currentDataset = parseInt($(this).data("idx"));
             updatePreferences();
         });
+    });
+    
+    $(document).on('input', '.line-data', function() {
+        var i = 0,
+            arr = [],
+            len = Options.line[Options.currentDataset].dataset.data.length;
+        
+        $('.line-data').each(function() {
+            var x = $(this).val();
+            if(x) {
+                arr[i++] = x;
+            }
+        });
+        
+        Options.line[Options.currentDataset].dataset.data = arr;
+        (arr.length === len) ? updateDataset() : recreateLineChart();
+    });
+    
+    $(document).on('blur', '.line-data', function() {
+        createDataNodes();
+    });
+
+    $('.line-data').each(function(idx) {
+        $(this).val(Options.line[Options.currentDataset].dataset.data[idx]);
     });
     
     $(window).resize(function() {
@@ -208,5 +264,6 @@ $(document).ready(function() {
     $("#linePoints").prop("checked", true);
     setLineColorChooserPosition();
     updateLineChart();
+    createDataNodes();
     
 });
